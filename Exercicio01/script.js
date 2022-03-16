05acd1a1f9d6e2682e09926b999e3a5923abb188
Diego Lima <xxdiegolsxx@gmail.com>
2022-03-16 15:52:09 -0300

    subindo exercicios

---
+++ Exercicio01/script.js
@@ -0,0 +1,11 @@
+function email() {
+    window.open("mailto:xxdiegolsxx@gmail.com"); 
+}
+
+function linkedin() {
+    window.open("https://www.linkedin.com/in/diegolimadev/");
+}
+
+function github() {
+    window.open("https://www.github.com/diegolimadev/");
+}
